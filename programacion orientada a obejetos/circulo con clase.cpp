6a73ae3fdd3bc4a66421f3975474ae0191af879b
edrei199 <edreialvarado519@gmail.com>
2025-05-30 21:29:43 -0600

    Add files via upload

---
+++ programacion orientada a obejetos/circulo con clase.cpp
@@ -0,0 +1,50 @@
+#include <conio.h>
+#include <stdio.h>
+#define pi 3.1416
+
+class circulo{
+	private:
+		float radio;
+	
+	public:
+		circulo(float r) : radio(r){}
+		
+		float calcularPerimetro(){
+			return 2 * pi * radio;
+		}
+		
+		float calcularArea(){
+			return pi * radio * radio;
+		}
+		
+		void setRadio(float r){
+			radio = r;
+		}
+		
+		float getRadio(){
+			return radio;
+		}
+};
+
+main()
+{
+	float radio;
+	char opcion;
+	
+	printf("Introdusca P para el perimetro o A para el area: ");
+	opcion = getch();
+	
+	printf("\nDame el radio: ");
+	scanf("%f", &radio);
+	
+	circulo miCirculo(radio);
+	
+	if(opcion == 'p'){
+		printf("\nEl perimetro es: %.2f", miCirculo.calcularPerimetro());
+	}
+	
+	else if(opcion == 'a'){
+		printf("\nEl area es: %.2f", miCirculo.calcularArea());
+	}
+	getch();	
+}
